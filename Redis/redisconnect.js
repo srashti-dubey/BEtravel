afcd6c828d42cddf0ab5cd3edba61186807b248b
Srashti-Dubey <srashti.dubey27@gmail.com>
2023-05-08 15:29:36 +0530

    add

--- Redis/redisconnect.js
+++ Redis/redisconnect.js
@@ -1,73 +1,73 @@
-const redis = require('redis')
-const test = require('../Model/City')
+// const redis = require('redis')
+// const test = require('../Model/City')
 
-let redisclient;
- (async () => {
+// let redisclient;
+//  (async () => {
 
-        redisclient = redis.createClient()
+//         redisclient = redis.createClient()
 
-        redisclient.on('error', (error) => {
+//         redisclient.on('error', (error) => {
 
-            console.log('error connecting redis' + error)
-        })
-        await redisclient.connect()
+//             console.log('error connecting redis' + error)
+//         })
+//         await redisclient.connect()
 
-    })()
-
-
-async function removeRedisData(cacheKey) {
-
-    try {
-        await redisclient.del(cacheKey)
-
-        console.log(`Removed ${cacheKey} from Redis cache`)
-
-    } catch (err) {
-
-        console.error(`Error removing ${cacheKey} from Redis cache:`, err)
-    }
-}
+//     })()
 
 
-async function getDataFromCacheOrMongoDB(cacheKey) {
+// async function removeRedisData(cacheKey) {
 
-    try {
-        const cacheData = null
-        await redisclient.get(cacheKey)
+//     try {
+//         await redisclient.del(cacheKey)
 
-        if (cacheData) {
+//         console.log(`Removed ${cacheKey} from Redis cache`)
 
-            console.log('cache hit', "heyyyy redis")
+//     } catch (err) {
 
-            const fetchedData = JSON.parse(cacheData)
+//         console.error(`Error removing ${cacheKey} from Redis cache:`, err)
+//     }
+// }
 
-            return fetchedData
 
-        } else {
+// async function getDataFromCacheOrMongoDB(cacheKey) {
 
-            console.log('cache miss')
+//     try {
+//         const cacheData = null
+//         await redisclient.get(cacheKey)
 
-            let fetchedData = await test.find();
+//         if (cacheData) {
 
-            await redisclient.set(cacheKey, JSON.stringify(fetchedData), 'EX', 3000)
+//             console.log('cache hit', "heyyyy redis")
 
-            return fetchedData
+//             const fetchedData = JSON.parse(cacheData)
 
-        }
+//             return fetchedData
 
-    } catch (err) {
+//         } else {
 
-        console.error(err)
+//             console.log('cache miss')
 
-        return null
-    }
-}
+//             let fetchedData = await test.find();
 
-module.exports = {
+//             await redisclient.set(cacheKey, JSON.stringify(fetchedData), 'EX', 3000)
 
-    getDataFromCacheOrMongoDB,
+//             return fetchedData
 
-    removeRedisData,
+//         }
 
-}
+//     } catch (err) {
+
+//         console.error(err)
+
+//         return null
+//     }
+// }
+
+// module.exports = {
+
+//     getDataFromCacheOrMongoDB,
+
+//     removeRedisData,
+
+// }
 
